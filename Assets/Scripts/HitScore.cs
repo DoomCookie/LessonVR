82e89bf666b04d2eee33e92c8785121501bda1c7
Artem <artem@mail.ru>
2022-04-01 17:51:29 +0300

    Мешень с подсчетом очков

---
+++ Assets/Scripts/HitScore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitScore : MonoBehaviour
+{
+    [SerializeField]
+    Transform centerPoint, bordrPoint;
+    [SerializeField]
+    int scoreBase = 100;
+    [SerializeField]
+    Text textScore;
+    float length;
+
+    private void Start()
+    {
+        length = (centerPoint.TransformPoint(centerPoint.position) - centerPoint.TransformPoint(bordrPoint.position)).magnitude;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Target")
+        {
+            Vector3 contact = collision.GetContact(0).point;
+            float lengthContact = (centerPoint.TransformPoint(contact) - centerPoint.TransformPoint(centerPoint.position)).magnitude / length;
+            float koef = (float)Math.Round((double)lengthContact, 2);
+            int score = Mathf.RoundToInt(scoreBase * (1 - koef));
+            textScore.text = score.ToString();
+            
+        }
+    }
+}
